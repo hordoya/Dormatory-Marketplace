7d0714d78df46c69b43fa7f20a517814b191483e
hordoya <hordoyabanik88@gmail.com>
2024-10-04 21:51:00 +0200

    i have updated ProductRepository

--- src/main/java/com/example/marketplace/marketplace/Repos/ProductRepository.java
+++ src/main/java/com/example/marketplace/marketplace/Repos/ProductRepository.java
@@ -11,8 +11,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByUser(User user);
 
-    //@Query("SELECT p FROM Product p WHERE p.user != :user")
-    //List<Product> findByUserNot(@Param("user") User user);
     List<Product> findAllByUserNot(User user);
 
     List<Product> findAllByUserNotAndInCartFalse(User user);
